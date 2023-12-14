4895bbd5e4350ae382c13b919815758d924dc57c
INJUST1C <voportno@gmail.com>
2023-12-14 14:38:27 +0100

    transfer to git

---
+++ src/main/java/com/study/PO/RecrutationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.study.PO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrutationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/PO/RecrutationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.PO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecrutationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
